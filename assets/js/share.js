0bd26ee5a81a781c49fb1e8a930f4e6d127ea981
klinowskimichal-maker <klinowskimichal@gmail.com>
2026-09-19 00:40:28 +0100

    Use fresh PSKL share URL without stale cache

--- assets/js/share.js
+++ assets/js/share.js
@@ -1,6 +1,6 @@
 (function(){
 'use strict';
-const site='https://klinowskimichal-maker.github.io/michal.project/udostepnij.html?v=61';
+const site='https://klinowskimichal-maker.github.io/michal.project/udostepnij.html?v=63';
 const status=document.getElementById('status');
 const isEN=window.PSKL_LANG==='en';
 const tr=(pl,en)=>isEN?en:pl;
