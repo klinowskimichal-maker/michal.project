717959984de5750f6353a844bd3ec276e93fd3b9
klinowskimichal-maker <klinowskimichal@gmail.com>
2026-09-18 06:27:33 +0100

    Share current PSKL v57 entry

--- assets/js/share.js
+++ assets/js/share.js
@@ -1,6 +1,6 @@
 (function(){
 'use strict';
-const site='https://klinowskimichal-maker.github.io/michal.project/udostepnij.html?v=56';
+const site='https://klinowskimichal-maker.github.io/michal.project/udostepnij.html?v=57';
 const status=document.getElementById('status');
 const isEN=window.PSKL_LANG==='en';
 const tr=(pl,en)=>isEN?en:pl;
